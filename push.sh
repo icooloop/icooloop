81ba227e03d1ff6c4878b083f72cd802441452db
libin <980324794@qq.com>
2020-09-07 13:49:51 +0800

    deploy

---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://github.com/icooloop/icooloop.git master
+
